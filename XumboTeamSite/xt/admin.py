6e5b4a7b92f5e7068ee159d515e54050da9504aa
joaoduarte <joaomaiaduarte@gmail.com>
2013-07-04 17:59:47 +0100

    multiple images in news

--- XumboTeamSite/xt/admin.py
+++ XumboTeamSite/xt/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from xt.models import Member, News
+from xt.models import Member, News, Image
 
 # Register your models here.
 admin.site.register(Member)
 admin.site.register(News)
+admin.site.register(Image)
--- XumboTeamSite/xt/models.py
+++ XumboTeamSite/xt/models.py
@@ -2,6 +2,7 @@
 # -*- coding: utf8 -*-
 
 from django.db import models
+from datetime import datetime   
 
 class Member(models.Model):
     #username = models.CharField(max_length=100)
@@ -14,15 +15,17 @@ class Member(models.Model):
         return unicode(self.name)
     
 class Image(models.Model):
-    photo = models.ImageField(upload_to='images', blank=True)
+    photo = models.ImageField(upload_to='images')
+    date_uploaded=models.DateTimeField(default=datetime.now())
     title=models.CharField(max_length=300, blank=True)
     description=models.TextField(blank=True)
+    def __unicode__(self):
+        return unicode(self.photo) #modificar para ter timestamp?
     
 class News(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('Data de publicação: ')
     text = models.TextField()
-    #images = models.ImageField(upload_to='news_images', blank=True)
     images=models.ManyToManyField(Image)
     def __unicode__(self):
         return unicode(self.title)
